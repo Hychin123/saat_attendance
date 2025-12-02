f687980bfd26c7a06331a42aca12868dd212c010
Hychin123 <hychin247@gmail.com>
2025-12-02 15:41:10 +0700

    Add login route for QR code authentication

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Login route - redirect to Filament admin
+Route::get('/login', function () {
+    return redirect('/admin/login');
+})->name('login');
+
 // Attendance routes
 Route::prefix('attendance')->group(function () {
     Route::get('/qr', [AttendanceController::class, 'showQrCode'])->name('attendance.qr');
